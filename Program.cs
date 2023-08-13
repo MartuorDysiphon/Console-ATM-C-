66e76f0c27bae5a0bd69f0a4043014e9597bf85f
MartuorDysiphon <129260867+MartuorDysiphon@users.noreply.github.com>
2023-08-13 14:09:39 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,102 +1,238 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
 using System.Linq;
-using System.Linq.Expressions;
-using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using System.Security;
 
-
-namespace Basic_ATM
+public class cardHolder
 {
-    class Intro
+    string cardNo;
+    int pin;
+    string firstName;
+    string lastName;
+    double balance;
+
+    public cardHolder(string cardNo, int pin, string firstName, string lastName, double balance)
     {
-        static int Main(string[] args)
+        this.cardNo = cardNo;
+        this.pin = pin;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+    }
+
+    //getters for cardHolder
+    public string getCardNo()
+    {
+        return cardNo;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public string getFirstName()
+    {
+        return firstName;
+    }
+
+    public string getLastName()
+    {
+        return lastName;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    //setters for cardHolder
+    public void setCardNo(string newCardNo)
+    {
+        cardNo = newCardNo;
+    }
+
+    public void setPin(int newPin)
+    {
+        pin = newPin;
+    }
+
+    public void setFirstName(string newFirstName)
+    {
+        firstName = newFirstName;
+    }
+
+    public void setLastName(string newLastName)
+    {
+        lastName = newLastName;
+    }
+
+    public void setBalance(double newBalance)
+    {
+        balance = newBalance;
+    }
+        
+    public static void Main(string[] args)
+    {
+        void homePrintOptions()
         {
-       
-            Console.WriteLine("Welcome To Martuor Dysiphon Banking Systems");
-            Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||");
-            Console.WriteLine("Press :");
-            Console.WriteLine("1: New Client");
-            Console.WriteLine("2: Existing Client");
-            
-            int value = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please Choose from one of the following options.......");
+            Console.WriteLine("1. Existing Client");
+            Console.WriteLine("2. New Client");
+            Console.WriteLine("3. Exit");
+        }
 
-            switch (value)
+
+
+        void printOptions()
+        {
+
+            //Console.WriteLine("");
+            Console.WriteLine("Please Choose from one of the following options.......");
+            Console.WriteLine("1. Deposit");
+            Console.WriteLine("2. Withdraw");
+            Console.WriteLine("3. Show Balance");
+            Console.WriteLine("4. Exit");
+        }
+
+        void deposit(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to deposit?: ");
+            double deposit = double.Parse(Console.ReadLine());
+            currentUser.setBalance(currentUser.getBalance() + deposit);
+            Console.WriteLine($"Thank you for your deposit. Your Current Balance : {currentUser.getBalance()} ");
+        }
+
+        void withdraw(cardHolder currentUser)
+        {
+            Console.WriteLine("How much money would you like to withdraw?: ");
+            double withdraw = double.Parse(Console.ReadLine()); 
+             
+            //VerificationException of funds
+            if (currentUser.getBalance() < withdraw)
             {
-                case 1:
-                    Console.WriteLine("To Open an account the following are required: ");
-                    Console.WriteLine("ID card: Proof of Address: Security No");
-                    Console.WriteLine("|||||||||||||||||||||||||||||||||||||||||||||||");
-                    Console.Write("Do you ave all te required documents|informations?; "); int nationality = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Press 1 if Yes| 2 if No");                    
+                Console.WriteLine("Insufficient Balance");
+            }
+            else
+            {
+                currentUser.setBalance(currentUser.getBalance() - withdraw);
+                Console.WriteLine("You successfully withdrawn money: You are good to go ");
+            }
+        }
 
-                    if (nationality < 2 )
+        void balance(cardHolder currentUser)
+        {
+            Console.WriteLine($"Your Current Balance: {currentUser.getBalance()} ");
+            
+        }
+
+        List<cardHolder> cardHolders = new List<cardHolder>();
+        cardHolders.Add(new cardHolder("11286312", 1234, "Amelia", "Adams", 1071.20));
+        cardHolders.Add(new cardHolder("13492671", 2468, "Jamal", "Samson", 668.81));
+        cardHolders.Add(new cardHolder("19348628", 3671, "Karabo", "Maila", 67.81));
+        cardHolders.Add(new cardHolder("13492671", 2468, "Jamal", "Samson", 3667.81));
+
+        Console.WriteLine("Welcome To Martuor Dysiphon Banking Systems:");
+        int homeOption = 0;
+        do
+        {
+            homePrintOptions();
+            try
+            {
+                homeOption = int.Parse(Console.ReadLine());
+            }
+            catch { }
+            if (homeOption == 1) 
+            {
+                cardHolders.Add(new cardHolder("11286312", 1234, "Amelia", "Adams", 1071.20));
+                cardHolder
+
+            }
+            else if (homeOption == 2) 
+            {
+                //prompt user Console.WriteLine("");
+                Console.WriteLine("Welcome To Martuor Dysiphon Banking Systems:");
+                Console.WriteLine("Please Enter your account Number: ");
+                string accountNumber = "";
+                cardHolder currentUser;
+
+                while (true)
+                {
+                    try
                     {
-                        Console.Write("Enter your ID number:"); long ID = long.Parse(Console.ReadLine());
-                        if(ID < 999999999999) 
+                        accountNumber = Console.ReadLine();
+                        //database verification
+                        currentUser = cardHolders.FirstOrDefault(a => a.cardNo == accountNumber);
+                        if (currentUser != null)
                         {
-                            Console.WriteLine("invalid ID");
-                            Console.Write("Please Enter  yout correct ID number:"); long ID0 = long.Parse(Console.ReadLine());
-                            if (ID0 < 999999999999)
-                            {
-                                Console.WriteLine("My Apoloies the followin ID numbers you are invalid");
-                                Console.WriteLine($"1. invalid ID: {ID} ");
-                                Console.WriteLine($"2. invalid ID: {ID0} ");
-                                Console.WriteLine($"Seems like we cannot help you since you provided invalid ID numbers");
-                            }
+                            break;
                         }
                         else
                         {
-                            Console.Write("Enter your FirstName(s): "); string firstName = Console.ReadLine();
-                            Console.Write("Enter your lastName: "); string lastName = Console.ReadLine();
-
-                            //Date of Birth
-                            int DD, MM, YYYY;
-                            Console.WriteLine("Enter your Date of Birth |YYYY|DD|MM|");
-                            Console.Write("Enter your Year of Birth: "); YYYY = int.Parse(Console.ReadLine());
-                            Console.Write("Enter your Day of Birth: "); DD = int.Parse(Console.ReadLine());
-                            Console.Write("Enter your Month of Birth: Choose from te bellow: "); MM = int.Parse(Console.ReadLine());
-                            Console.WriteLine("1. January");
-                            Console.WriteLine("2. February");
-                            Console.WriteLine("3. March");
-                            Console.WriteLine("4. April");
-                            Console.WriteLine("5. May");
-                            Console.WriteLine("6. June");
-                            Console.WriteLine("7. July");
-                            Console.WriteLine("8. August");
-                            Console.WriteLine("9. September");
-                            Console.WriteLine("10. October");
-                            Console.WriteLine("11. November");
-                            Console.WriteLine("12. December");
-
-
+                            Console.WriteLine("Account not reconised. Enter a valid Account Number!");
                         }
                     }
-                    else
-                        Console.Write("Please Enter your Passport or Internation Identification Number:");
-                        long  ininterID = long.Parse(Console.ReadLine());
 
+                    catch
+                    {
+                        Console.WriteLine("Account not reconised. Enter a valid Account Number!");
+                    }
+                }
 
-                   break; /*breaking case 1*/
+                Console.WriteLine("Please Enter your account Pin : ");
+                int userPin = 0;
+                while (true)
+                {
+                    try
+                    {
+                        userPin = int.Parse(Console.ReadLine());
+                        //database verification
+                        if (currentUser.getPin() == userPin)
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Incorrect Pin. Please Enter a valid Pin!");
+                        }
+                    }
 
-                case 2:
-                    Console.WriteLine("Ellow tere");
+                    catch
+                    {
+                        Console.WriteLine("Incorrect Pin. Please Enter a valid Pin!");
+                    }
+                }
 
-                    break;
-
-                
-               
-                default:
-                    Console.WriteLine("Sorry Invalid Option!");
-                    break;
+                Console.WriteLine($"Welcome {currentUser.getFirstName()} :) ");
+                int option = 0;
+                do
+                {
+                    printOptions();
+                    try
+                    {
+                        option = int.Parse(Console.ReadLine());
+                    }
+                    catch { }
+                    if (option == 1) { deposit(currentUser); }
+                    else if (option == 2) { withdraw(currentUser); }
+                    else if (option == 3) { balance(currentUser); }
+                    else { option = 0; }
+                }
+                while (option != 4);
+                Console.WriteLine("Thank you, Have an amazing day :) ");
             }
-            return value;
+            else { homeOption = 0; }
         }
-       
+        while (homeOption != 4);
+        Console.WriteLine("Thank you, Have an amazing day :) ");
+
         
+
+
     }
 
 }
-
